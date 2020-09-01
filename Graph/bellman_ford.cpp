56b7f91421e66d7bb61a151dc15b46f8bd4c1f86
aksoumya99 <49592393+aksoumya99@users.noreply.github.com>
2020-09-01 18:37:37 +0530

    Create bellman_ford.cpp

---
+++ Graph/bellman_ford.cpp
@@ -0,0 +1,26 @@
+typedef long long ll;
+typedef pair<ll,ll > pll;
+#define fi first
+#define se second
+const ll inf=1e7;
+ll bellman_ford(vector<pair<ll,pll > > &edges,ll dest,ll src,ll v,ll e){
+    ll i,j,k;
+    vector<ll > dist(inf);
+    for(i=1;i<=v-1;i++){
+        for(j=0;j<e;j++){
+            ll u=edges[j].fi,v=edges[j].se.fi,w=edges[j].se.se;
+            if(dist[u]!=inf&&dist[v]>dist[u]+w){
+                dist[v]=dist[u]+w;
+            }
+        }
+    }
+    //check for negative cycles
+    for(j=0;j<e;j++){
+        ll u=edges[j].fi,v=edges[j].se.fi,w=edges[j].se.se;
+        if(dist[u]!=inf&&dist[v]>dist[u]+w){
+            cout<<"Negative cycle\n";
+            return -1;
+        }
+    }
+    return dist[dest];
+}
