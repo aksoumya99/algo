29d63665ca8ee5c704935a4a54752598cfcc61ee
aksoumya99 <49592393+aksoumya99@users.noreply.github.com>
2020-09-05 19:46:43 +0530

    Update bellman_ford.cpp

--- Graph/bellman_ford.cpp
+++ Graph/bellman_ford.cpp
@@ -5,7 +5,7 @@ typedef pair<ll,ll > pll;
 const ll inf=1e7;
 ll bellman_ford(vector<pair<ll,pll > > &edges,ll dest,ll src,ll v,ll e){
     ll i,j,k;
-    vector<ll > dist(inf);
+    vector<ll > dist(v,inf);
     for(i=1;i<=v-1;i++){
         for(j=0;j<e;j++){
             ll u=edges[j].fi,v=edges[j].se.fi,w=edges[j].se.se;
